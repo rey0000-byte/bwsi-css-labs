5c562ea65a6420fc1645621187d3a5c8b5bba086
rey0000-byte <rey0000mc@gmail.com>
2026-03-23 22:19:15 -0400

    Change robot speed from 8 m/s to 3 m/s

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -5,7 +5,7 @@ The first lab in the BWSI CSS course. To complete this lab, fill out the variabl
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
 
-This is to simulate a change made on a robot: robot_speed = 8 # m/s
+This is to simulate a change made on a robot: robot_speed = 3 # m/s
 
 def main():
     print("Hello World!")
